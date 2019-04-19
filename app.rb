884297a33c5789fe0e63b8c9dc0fd8bdd7b02208
Jennie Kerfstedt <jenniekerf@gmail.com>
2019-04-19 15:11:49 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -10,7 +10,7 @@ get '/new' do
   erb :create_puppy
 end 
 
-get '/puppy' do
+post '/puppy' do
  @puppy = Puppy.new(params['name'], params['breed'], params['months_old'])
     erb :display_puppy
 end 
